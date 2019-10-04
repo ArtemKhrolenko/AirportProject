6973bfdd3d90d0eef7c57817518b16deeecaa613
ron_in <roninorgua@gmail.com>
2019-10-04 09:39:36 +0300

    Не работает выводданных

---
+++ AirportPrj/AirportPrj/Model/Passenger.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportPrj.Model
+{
+    class Passenger : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _nationality;
+        private string _passport;
+        private DateTime? _dateOfBirth;
+        private Sex _sex;
+        private PassClass _passClass;
+        private string _flightID;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if (value == _firstName) return;
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (value == _lastName) return;
+                _lastName = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Nationality
+        {
+            get => _nationality;
+            set
+            {
+                if (value == _nationality) return;
+                _nationality = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Passport
+        {
+            get => _passport;
+            set
+            {
+                if (value == _passport) return;
+                _passport = value;
+                OnPropertyChanged();
+            }
+        }
+        public DateTime? DateOfBirth
+        {
+            get => _dateOfBirth;
+            set
+            {
+                if (value.Equals(_dateOfBirth)) return;
+                _dateOfBirth = value;
+                OnPropertyChanged();
+            }
+        }
+        public Sex Sex
+        {
+            get => _sex;
+            set
+            {
+                if (value == _sex) return;
+                _sex = value;
+                OnPropertyChanged();
+            }
+        }
+        public PassClass PassClass
+        {
+            get => _passClass;
+            set
+            {
+                if (value == _passClass) return;
+                _passClass = value;
+                OnPropertyChanged();
+            }
+        }
+        public string FlightID
+        {
+            get => _flightID;
+            set
+            {
+                if (value == _flightID) return;
+                _flightID = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    enum Sex
+    {
+        Male,
+        Female
+    }
+
+    enum PassClass
+    {
+        Business,
+        Economy
+    }
+}
---
+++ AirportPrj/AirportPrj/ViewModel/PassengerTabViewModel.cs
@@ -0,0 +1,124 @@
+﻿using AirportPrj.DataBase;
+using AirportPrj.Model;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using Microsoft.Win32;
+
+namespace AirportPrj.ViewModel
+{
+    class PassengerTabViewModel : ViewModelBase
+    {
+
+        public PassengerTabViewModel(AirportContext context)
+        {
+            Context = context;
+            Context.Passengers.Load();
+        }
+
+        public AirportContext Context { get; }
+        public Passenger PassengertInfo { get; set; } = new Passenger();
+        public Passenger SelectedPassenger { get; set; }
+
+        #region Commands
+        private RelayCommand _addPassengerCommand;
+        private RelayCommand _updatePassengerCommand;
+        private RelayCommand _deletePassengerCommand;
+        private RelayCommand _passengerGridSelectionChangedCommand;
+
+
+        public ICommand AddPassengerCommand => _addPassengerCommand ??
+                    (_addPassengerCommand = new RelayCommand(
+                        () =>
+                        {
+                            Context.Passengers.Add(new Passenger()
+                            {
+                                FirstName = PassengertInfo.FirstName,
+                                LastName = PassengertInfo.LastName,
+                                Nationality = PassengertInfo.Nationality,
+                                Passport = PassengertInfo.Passport,
+                                DateOfBirth = PassengertInfo.DateOfBirth,
+                                Sex = PassengertInfo.Sex,
+                                PassClass = PassengertInfo.PassClass,
+                                FlightID = PassengertInfo.FlightID,
+                            });
+                            Context.SaveChanges();
+                        },
+                        () =>
+                        {
+                            if (string.IsNullOrEmpty(PassengertInfo.FirstName)
+                            || string.IsNullOrEmpty(PassengertInfo.LastName)
+                            || PassengertInfo.Passport == null)
+                            {
+                                return false;
+                            }
+                            return true;
+                        }));
+
+        public ICommand UpdatePassengerCommand =>
+            _updatePassengerCommand ??
+            (_updatePassengerCommand = new RelayCommand(
+                () =>
+                {
+                    SelectedPassenger.FirstName = PassengertInfo.FirstName;
+                    SelectedPassenger.LastName = PassengertInfo.LastName;
+                    SelectedPassenger.Nationality = PassengertInfo.Nationality;
+                    SelectedPassenger.Passport = PassengertInfo.Passport;
+                    SelectedPassenger.DateOfBirth = PassengertInfo.DateOfBirth;
+                    SelectedPassenger.Sex = PassengertInfo.Sex;
+                    SelectedPassenger.PassClass = PassengertInfo.PassClass;
+                    SelectedPassenger.FlightID = PassengertInfo.FlightID;
+                    Context.SaveChanges();
+                },
+                () =>
+                {
+                    if (SelectedPassenger == null) return false;
+                    if (string.IsNullOrEmpty(PassengertInfo.FirstName)
+                        || string.IsNullOrEmpty(PassengertInfo.LastName)
+                        || PassengertInfo.Passport == null)
+                    {
+                        return false;
+                    }
+                    return true;
+                }));
+
+        public ICommand DeletePassengerCommand =>
+            _deletePassengerCommand ??
+            (_deletePassengerCommand = new RelayCommand(
+                () =>
+                {
+                    Context.Passengers.Remove(SelectedPassenger);
+                    Context.SaveChanges();
+                },
+                () => SelectedPassenger != null));
+
+        public ICommand PassengerGridSelectionChangedCommand =>
+            _passengerGridSelectionChangedCommand ??
+            (_passengerGridSelectionChangedCommand =
+                new RelayCommand(
+                    () =>
+                    {
+                        PassengertInfo.FirstName = SelectedPassenger.FirstName;
+                        PassengertInfo.LastName = SelectedPassenger.LastName;
+                        PassengertInfo.Nationality = SelectedPassenger.Nationality;
+                        PassengertInfo.Passport = SelectedPassenger.Passport;
+                        PassengertInfo.DateOfBirth = SelectedPassenger.DateOfBirth;
+                        PassengertInfo.Sex = SelectedPassenger.Sex;
+                        PassengertInfo.PassClass = SelectedPassenger.PassClass;
+                        PassengertInfo.FlightID = SelectedPassenger.FlightID;
+                    },
+                    () => SelectedPassenger != null));
+
+        #endregion
+
+    }
+}
